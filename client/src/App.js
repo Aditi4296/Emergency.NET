f26111e3308f12438dd9fcdfdf5903a095885647
suvigyagarg <suvigya.2003.garg@gmail.com>
2023-09-23 11:30:58 +0530

    Intialised the starting files for the project

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Footer from './components/footer/footer';
+import Navbar from './components/navbar/navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <h1>React App</h1>
+    <Footer/>
+    </div>
+  );
+}
+
+export default App;
